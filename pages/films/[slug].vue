66f79eff4c7b343b5d7072334cc4aca24b8413de
Nebelschwimmer <nebelschwimmer@gmail.com>
2025-06-30 23:56:07 +0300

    style: sidebar card2

--- pages/films/[slug].vue
+++ pages/films/[slug].vue
@@ -5,7 +5,7 @@
             <Meta name="description" :content="film?.description" />
         </Head>
         <NuxtLayout name="right-drawer">
-            <v-card class="mt-2">
+            <v-card variant="text" class="mt-2">
                 <v-card-title>{{
                     $t("pages.films.related_persons")
                 }}</v-card-title>
