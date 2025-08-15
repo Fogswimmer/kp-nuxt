c9dc40ff2634ae80370735c878c20bc708f54e62
Fogswimmer <fgswimmer@gmail.com>
2025-08-15 23:02:55 +0300

    refactor: mobile header

--- components/App/AppHeader.vue
+++ components/App/AppHeader.vue
@@ -1,18 +1,35 @@
 <template>
-	<v-app-bar order="0" scroll-behavior="hide">
+	<v-app-bar
+		order="0"
+		scroll-behavior="hide"
+		:extended="$vuetify.display.smAndDown && showSearch"
+	>
 		<v-app-bar-title>
 			<Logo />
 		</v-app-bar-title>
-		<v-responsive max-width="600">
+		<v-responsive v-if="$vuetify.display.mdAndUp" max-width="600">
 			<AppSearch :query="searchQuery" @update:query="handleUpdateSearch"
 		/></v-responsive>
 		<v-spacer v-if="$vuetify.display.mdAndUp" />
 		<template #append>
 			<div class="d-flex ga-1 align-center mr-2">
 				<ProfileNav v-if="$vuetify.display.mdAndUp" />
+				<v-btn
+					v-if="$vuetify.display.smAndDown"
+					icon="mdi-magnify"
+					@click="showSearch = !showSearch"
+				/>
 				<SettingsBtn
 					:active-theme="activeTheme"
 					@update:active-theme="$emit('update:activeTheme', $event)"
+				/>
+			</div>
+		</template>
+		<template v-if="$vuetify.display.smAndDown && showSearch" #extension>
+			<div class="w-100 pa-2 mb-2">
+				<AppSearch
+					:query="searchQuery"
+					@update:query="handleUpdateSearch"
 				/>
 			</div>
 		</template>
@@ -26,6 +43,7 @@ import SettingsBtn from "../Containment/Btns/SettingsBtn.vue";
 import AppSearch from "./AppSearch.vue";
 
 const searchQuery = ref<string>("");
+const showSearch = ref<boolean>(false);
 
 const localeRoute = useLocaleRoute();
 
